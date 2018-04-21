6f66872ee971b878d31e9a470fdfd7d6528b839b
Norrismi <Norrismi0@gmail.com>
2018-04-20 20:02:24 -0400

    auth complete

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,19 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
+
+const User = mongoose.model("users"); //single argument pulls data out of mongo
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -10,11 +23,13 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("access token", accessToken);
-      console.log("refresh token", refreshToken);
-      console.log("profile", profile);
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        existingUser
+          ? done(null, existingUser)
+          : new User({ googleId: profile.id }) // creates new model instance
+              .save()
+              .then(user => done(null, user));
+      });
     }
   )
 );
-
-
